19a7816100b0f91a9765ee879e57e2ad0afcad30
Syed Huzaifa Ali <k250128@nu.edu.pk>
2026-09-09 21:18:22 +0500

    Add function to check for duplicate numbers

---
+++ 3.py
@@ -0,0 +1,13 @@
+def contains_duplicate(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+
+nums = [1, 2, 3, 4, 5, 2]
+print(contains_duplicate(nums))
